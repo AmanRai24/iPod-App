e2ea22854021a9ab6e8e54c5ea79d3e6e53a752b
Aman Rai <arai24197@gmail.com>
2020-08-13 19:54:56 +0530

    Updates

---
+++ precache-manifest.97dc5b256744c0783a7d436aceb00bab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdd6e4036d742b800d9af13a8c8387a4",
+    "url": "/iPod-App/index.html"
+  },
+  {
+    "revision": "237bbdb399e2053e42ef",
+    "url": "/iPod-App/static/css/main.cfc25f22.chunk.css"
+  },
+  {
+    "revision": "e7a9f353d2267beb4f48",
+    "url": "/iPod-App/static/js/2.f87b9c93.chunk.js"
+  },
+  {
+    "revision": "3b6069e01f0db714f9e787b4dbe9775c",
+    "url": "/iPod-App/static/js/2.f87b9c93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "237bbdb399e2053e42ef",
+    "url": "/iPod-App/static/js/main.cbfcb2c7.chunk.js"
+  },
+  {
+    "revision": "a08e96a2d069b289e76f",
+    "url": "/iPod-App/static/js/runtime-main.6cc73831.js"
+  }
+]);
